3be89b9920d87bd788c07d810f2efe8ff129c864
Alexandre Grigorovitch <alexezh@gmail.com>
2025-03-09 10:24:45 -0700

    add tlsh

---
+++ histogramtextsignature.ts
@@ -0,0 +1,27 @@
+/**
+ * The idea is to generate histogram of words, then quantize the count.
+ * One challenge is low frequency entries especially with count 1; they might completely
+ * disappear across documents. We are going to  
+ */
+export class HistogramTextSignature {
+  private _histogram = new Map<number, number>();
+
+  public addWord(w: string): void {
+    let v = this._histogram.get(w);
+    if (v === undefined) {
+      v = 1;
+    } else {
+      v++;
+    }
+    this._histogram.set(w, v);
+  }
+
+  public computeKey(): string {
+    let lowFreq = 0;
+    for (let [_, c] of this._histogram) {
+      if (c < )
+    }
+    return "";
+  }
+}
+
